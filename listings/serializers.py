eb00222f5add6dee843146689890e0c25a2b06e5
Michael Busch <buschm2rpi@gmail.com>
2021-10-03 22:35:23 -0700

    add listing serializer

---
+++ listings/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Listing
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = (
+            'realtor',
+            'title',
+            'address',
+            'city',
+            'state',
+            'zipcode',
+            'description',
+            'price',
+            'bedrooms',
+            'bathrooms',
+            'garage',
+            'sqft',
+            'lot_size',
+            'photo_main',
+            'photo_1',
+            'photo_2',
+            'photo_3',
+            'photo_4',
+            'photo_5',
+            'photo_6',
+            'is_published',
+            'list_date'
+            )
